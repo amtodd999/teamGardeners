c1c3bea8be297c36eed502e838512bdbf40e1b8b
Angie Todd <a.m.todd999@gmail.com>
2021-07-10 09:33:03 -0400

    testing git push

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Team Gardeners
         </a>
       </header>
     </div>
